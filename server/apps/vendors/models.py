8ecdddf1d77ea395bb7b0773a337056788ecdea2
meuorii <fontillaskian@gmail.com>
2026-03-01 22:00:42 +0800

    Add is_approved field to VendorsStall model for admin stall approval

--- server/apps/vendors/models.py
+++ server/apps/vendors/models.py
@@ -4,12 +4,13 @@ from django.utils import timezone
 # Create your models here.
 class VendorsStall(models.Model):
     id = models.BigAutoField(primary_key=True)
-    vendor = models.ForeignKey('users.UsersVendorprofile', models.DO_NOTHING)
+    vendor = models.ForeignKey('users.UsersVendorprofile', models.DO_NOTHING, null=True, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     logo = models.ImageField(upload_to='vendors/logos/', blank=True, null=True)
     banner = models.ImageField(upload_to='vendors/banners/', blank=True, null=True)
     is_open = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
